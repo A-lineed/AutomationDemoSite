fb71396090503de418d18c5c91ad027dbcfd1fae
A-lineed <aline.edvania.franca@outlook.com>
2023-03-10 18:08:28 -0300

    Module WebTable
    Visit webtable and select number of items per page

---
+++ cypress/integration/webTable.spec.cy.js
@@ -0,0 +1,22 @@
+/// <reference types="Cypress" />
+
+describe('Login', () => {
+    Cypress.on('uncaught:exception', (err, runnable) => {
+        return false;
+    });
+
+    beforeEach(() => {
+        cy.visit('https://demo.automationtesting.in/Register.html')
+    })
+    it('Visita WebTable', () => {
+        cy.get('.nav > :nth-child(3) > a').click()
+        cy.title('Web Table')
+    })
+
+    it('Seleciona quantidade de itens por página', () => {
+        cy.get('.nav > :nth-child(3) > a').click()
+        cy.title('Web Table')
+        cy.get('.ui-grid-pager-row-count-picker > .ng-pristine').select('30')
+    })
+
+})
