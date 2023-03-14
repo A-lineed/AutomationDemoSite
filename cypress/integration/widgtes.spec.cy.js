4a55320fd6034fecf880f762e6aaf011e7959ae3
A-lineed <aline.edvania.franca@outlook.com>
2023-03-14 17:43:09 -0300

    Datepicker2

--- cypress/integration/widgtes.spec.cy.js
+++ cypress/integration/widgtes.spec.cy.js
@@ -37,7 +37,7 @@ describe('AutoComplete', () => {
     })
 })
 
-describe.only('Datepicker', () => {
+describe('Datepicker1', () => {
     beforeEach(() => {
         cy.visit('https://demo.automationtesting.in/Register.html')
         cy.get(':nth-child(5) > .dropdown-menu > :nth-child(3) > a').click()
@@ -48,10 +48,34 @@ describe.only('Datepicker', () => {
         cy.get('div#ui-datepicker-div').should('be.visible');
     })
 
-    it.only('Select date', () => {
+    it('Select date', () => {
         cy.get('input#datepicker1').click()
         cy.selectYear(2022)
         cy.selectMonth('January')
         cy.selectDay(17)
     })
+})
+
+describe('Datepicker2', () => {
+    beforeEach(() => {
+        cy.visit('https://demo.automationtesting.in/Register.html')
+        cy.get(':nth-child(5) > .dropdown-menu > :nth-child(3) > a').click()
+    })
+
+    it('Open date picker', () => {
+        cy.get('input#datepicker2').click()
+        cy.get('div.datepick-month').should('be.visible');
+    })
+    it('Clear data', () => {
+        cy.get('input#datepicker2').click()
+        cy.get('div.datepick-month-header select.datepick-month-year[title="Change the month"]').select('February')
+        cy.get('div.datepick-month-header select[title="Change the year"]').select('2023')
+        cy.get('div.datepick-ctrl a[title="Clear all the dates"]').click()
+    })
+
+    it('Close picker', () => {
+        cy.get('input#datepicker2').click()
+        cy.get('div.datepick-ctrl a[title="Close the datepicker"]').click()
+    })
+
 })
